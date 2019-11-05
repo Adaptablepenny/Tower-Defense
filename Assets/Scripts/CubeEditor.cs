f0670461a8783faf07a4dda97e0feab809dc008e
Michael Rosique <michaelrosique@gmail.com>
2019-11-05 09:58:14 -0600

    Created a CubeEditor script that makes the cube snap to the grid every 10 units on X and Z, also added a text label on the cube that gets its coordinates

---
+++ Assets/Scripts/CubeEditor.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class CubeEditor : MonoBehaviour
+{
+
+    [Range(1f, 20f)] [SerializeField] float gridSize = 10f;
+
+    TextMesh textMesh;
+    Vector3 snapPos;
+
+    void Start()
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        CubeSnaping();
+        CoordinateText();
+
+    }
+
+    private void CoordinateText()
+    {
+        textMesh = GetComponentInChildren<TextMesh>();
+        textMesh.text = snapPos.x / 10f + "," + snapPos.z / 10f;
+    }
+
+    private void CubeSnaping()
+    {
+        
+        snapPos.x = Mathf.RoundToInt(transform.position.x / 10f) * gridSize;
+        snapPos.z = Mathf.RoundToInt(transform.position.z / 10f) * gridSize;
+        transform.position = new Vector3(snapPos.x, 0f, snapPos.z);
+    }
+}
