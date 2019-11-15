55bf643e78dcebd18e72c00e7837c79aec8e2b5f
Michael Rosique <michaelrosique@gmail.com>
2019-11-14 21:34:26 -0600

    Lowered the level, disabled cube snapping in Cube Editor Script

--- Assets/Scripts/CubeEditor.cs
+++ Assets/Scripts/CubeEditor.cs
@@ -24,7 +24,7 @@ public class CubeEditor : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        CubeSnaping();
+        //CubeSnaping();
         LabelText();
         
 
